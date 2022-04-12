b925eb64a31c3b9321987c3d12d273895e527c13
Immaridel <cerbdog@gmail.com>
2022-04-11 23:00:40 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-# https://github.com/Rapptz/discord.py/blob/async/examples/reply.py
 import os
 import discord
 import asyncio
@@ -7,7 +6,6 @@ import platform
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-#TOKEN = "NDcyMjA4NzM4NjYxNDMzMzQ1.DjwC2Q.6luhJbgkN1NjAUBxr0Yn5-82DFI"
 TOKEN = "NDcyMjA4NzM4NjYxNDMzMzQ1.W1pwqA.H0-nE4NW8JHQfUvioElIxtCgtHc"
 
 #client = discord.Client()
@@ -19,19 +17,19 @@ async def on_message(message):
 	if message.author == client.user:
 		return
 
-	if message.content.startswith('gb ping'):
+	if message.content.startswith('.gping'):
 		msg = 'Oh my God. Fuck off, {0.author.mention}.'.format(message)
 		await message.channel.send(msg)
 
-	if message.content.startswith('gb echo'):
+	if message.content.startswith('.gecho'):
 		msg = message.content
-		await message.channel.send(message.channel, msg)
+		await message.channel.send(msg)
 
-	if message.content.startswith('gb help'):
+	if message.content.startswith('.ghelp'):
 		msg = "There's no help to offer.  Fuck off.\nhttps://giphy.com/gifs/QGzPdYCcBbbZm"
 		msg2 = "Also, I'm touching myself right now, and you cannot un-know that."
-		await message.channel.send(message.author, msg)
-		await message.channel.send(message.author, msg2)
+		await message.channel.send(msg)
+		await message.channel.send(msg2)
 
 @client.event
 async def on_ready():
